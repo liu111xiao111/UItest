4a639339e22f7139df89867378982e39c9e85260
liufei34 <liufei34@wanda.cn>
2016-05-12 16:21:46 +0800

    commit smart_life

---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ffan/smart_life_page.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+#sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+
+
+from com.qa.automation.appium.pages.ffan.smart_life_page_configs import *
+from com.qa.automation.appium.pages.common.super_page import *
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+
+#   进入应用的首页,是进入其他页面的入口
+class SmartLifePage(SuperPage):
+
+    def __init__(self,testcase,driver,logger):
+        super(SmartLifePage, self).__init__(testcase = testcase , driver = driver,logger = logger);
+
+    '''
+        usage : 进入慧生活,检查ffan logo
+    '''
+    def validSelf(self):
+        API().assert_view_by_text_android(testcase = self.testcase , driver = self.driver,
+                                          logger = self.logger ,
+                                          text=SmartLifePageConfigs.resource_id__iv_logo__iv);
+
+    '''
+        usage : 进入慧生活,点击全城找店
+    '''
+    def clickFindStore(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.text_findStore);
+
+    '''
+        usage : 进入慧生活,点击排队
+    '''
+    def clickQueue(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.text_queue);
+
+    '''
+        usage : 进入慧生活,点击飞悦
+    '''
+    def clickFly(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.text_fly);
+
+    '''
+        usage : 进入慧生活,点击优惠券
+    '''
+    def clickCoupon(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.text_coupon);
+
+    '''
+        usage : 进入慧生活,点击活动
+    '''
+    def clickActivity(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.text_activity);
+
+    '''
+        usage : 进入慧生活,点击活动列表
+    '''
+    def clickActivityTag(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.tag_activity);
+
+
+    '''
+        usage : 进入慧生活,点击优惠列表
+    '''
+    def clickCouponTag(self):
+        API().click_view_by_text_android(driver = self.driver,logger=self.logger,text=SmartLifePageConfigs.tag_coupon);
+
+
+if __name__ == '__main__':
+    pass;
---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ffan/smart_life_page_configs.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os,sys
+
+
+class SmartLifePageConfigs():
+
+    # 常量命名规则 resource_id指明资源类型是resource id;iv_center_tab view id name;iv指明view类型image view
+    # ffan logo图标
+    resource_id__iv_logo__iv = "iv_logo";
+
+    # text指明类型为text label
+    text_findStore = "全城找店";
+    text_queue = "排队";
+    text_fly = "飞悦";
+    text_coupon = "优惠券";
+    text_activity = "活动";
+    tag_activity = "活动";
+    tag_coupon = "优惠";
+
+
+
+
+    def __init__(self):
+        pass;
+
