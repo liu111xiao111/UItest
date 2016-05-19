b5be31e84be409017725b7782b61e9328a60bbd3
maguowei1 <maguowei1@wanda.cn>
2016-05-19 16:48:14 +0800

    增加爱逛街相关10条用例

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/testsuite_runner/wanglei_testsuite.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/testsuite_runner/wanglei_testsuite.py
@@ -6,6 +6,7 @@ import sys,os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))))))
 
+from com.qa.automation.appium.cases.ffan.myffan_cases import MyFfanCases
 from com.qa.automation.appium.cases.ffan.love_shopping_cases import LoveShoppingCases
 
 import unittest
@@ -23,6 +24,8 @@ if not os.path.exists(reportpath):
 
 # suite = unittest.TestLoader().loadTestsFromTestCase(MyFfanCases)
 suite = unittest.TestSuite()
+suite.addTest(MyFfanCases("test_login"))
+suite.addTest(MyFfanCases("test_logout"))
 suite.addTest(LoveShoppingCases("test_shopping_mall"))
 suite.addTest(LoveShoppingCases("test_film"))
 suite.addTest(LoveShoppingCases("test_food"))
