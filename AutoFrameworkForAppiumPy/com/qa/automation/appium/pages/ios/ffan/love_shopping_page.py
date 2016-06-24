07260f67860b8e3e309dd07967a8360bbf8a5358
maguowei1 <maguowei1@wanda.cn>
2016-06-24 10:35:48 +0800

    增加ios飞凡爱逛街页面及其配置实现。

---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/love_shopping_page.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+from time import sleep
+import unittest
+from com.qa.automation.appium.configs.driver_configs import *
+from com.qa.automation.appium.api.api import *
+from com.qa.automation.appium.pages.ios.common.ios_super_page import *
+from com.qa.automation.appium.pages.ios.ffan.love_shopping_page_configs import *
+
+from appium import webdriver
+
+# Returns abs path relative to this file and not cwd
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+
+
+"""
+    爱逛街tab对应页面实现
+"""
+
+
+class LoveShoppingPage(IosSuperPage):
+
+    def __init__(self, test_case, driver, logger):
+        super(LoveShoppingPage, self).__init__(testcase=test_case, driver=driver, logger=logger);
+
+    '''
+        usage : 进入到爱逛街页,检查购物中心按钮
+    '''
+
+    def validSelf(self):
+        API().assert_view_by_resourceID_Until(testcase=self.testcase,driver=self.driver,
+                                              logger=self.logger,resource_id=LoveShoppingPageConfigs.name_shopping_mall)
+
+
+    # 点击购物中心按钮
+    def clickOnShoppingMall(self):
+        API().click_view_by_resourceID(testcase=self.testcase,driver=self.driver,
+                                               logger=self.logger,resource_id=LoveShoppingPageConfigs.name_shopping_mall)
+        # API().click_view_by_text_android(testcase=self.testcase, driver=self.driver, logger=self.logger,
+        #                                  text=LoveShoppingPageConfigs.text_shopping_mall);
+
+    # 点击电影按钮
+    def clickOnFilm(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_film)
+
+    # 点击美食按钮
+    def clickOnFood(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_food)
+
+    # 点击品牌按钮
+    def clickOnBrand(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_brand)
+
+    # 点击亲子按钮
+    def clickOnChlidren(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_children)
+
+    # 点击优惠按钮
+    def clickOnPreferential(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_preferential)
+
+    # 点击购物按钮
+    def clickOnShopping(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_shopping)
+
+    # 点击限时抢购按钮
+    def clickOnFlashSale(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_flash_sale)
+
+    # 点击停车按钮
+    def clickOnParking(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_parking)
+
+    # 点击乐付按钮
+    def clickOnLePays(self):
+        API().click_view_by_resourceID(testcase=self.testcase, driver=self.driver,
+                                               logger=self.logger,
+                                               resource_id=LoveShoppingPageConfigs.name_le_pays)
+
+
+if __name__ == '__main__':
+    pass;
---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/love_shopping_page_configs.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+
+class LoveShoppingPageConfigs():
+
+
+    """
+        *********************************iOS 控件name*****************************
+
+    """
+    name_shopping_mall = "购物中心";
+    name_film = "电影";
+    name_food = "美食";
+    name_brand = "品牌";
+    name_children = "亲子";
+    name_preferential = "优惠";
+    name_shopping = "购物";
+    name_flash_sale = "限时抢购";
+    name_parking = "停车";
+    name_le_pays = "乐付";
+
+    name_search_all_city = "全城搜索";
+
+
+
+
+
+    def __init__(self):
+        pass;
