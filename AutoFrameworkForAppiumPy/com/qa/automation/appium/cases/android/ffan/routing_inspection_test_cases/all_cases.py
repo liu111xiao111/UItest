ac731228cb566df85b08725bcef5e836f90960a9
maguowei1 <maguowei1@wanda.cn>
2016-07-12 13:10:12 +0800

    增加巡检用例all cases
    Change-Id: I03d1d3ebd4aae8091246ca82daffe57aca2f2039

---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/routing_inspection_test_cases/all_cases.py
@@ -0,0 +1,47 @@
+# -*- coding:utf-8 -*-
+
+import sys,os
+
+import time
+from unittest import TestCase
+from unittest import TestLoader
+
+import HTMLTestRunner
+from unittest.suite import TestSuite 
+        
+from com.qa.automation.appium.cases.android.ffan.routing_inspection_test_cases.advertisement.hui_life_resource_niche_cases import HuiLifeResourceNicheCases
+        
+from com.qa.automation.appium.cases.android.ffan.routing_inspection_test_cases.advertisement.\
+        square_resource_niche_cases import SquareResourceNicheCases
+        
+from com.qa.automation.appium.cases.android.ffan.routing_inspection_test_cases.cinema. \
+         movie_ticket_cases import MovieTicketCases
+         
+from com.qa.automation.appium.cases.android.ffan.routing_inspection_test_cases.cinema. \
+         square_movie_cases import SquareMovieCases
+         
+from com.qa.automation.appium.cases.android.ffan.routing_inspection_test_cases.coupon_and_activity.activity_sharing_cases import ActivitySharingCases
+        
+
+if __name__ == "__main__":
+    build_num = sys.argv[1]
+    reportpath = "%s/report/ffan/%s/%s/" % ("/Users/maguowei/autotest/AutoFrameworkForAppiumPy", time.strftime("%Y%m%d"), build_num)
+    if not os.path.exists(reportpath):
+        os.makedirs(reportpath)
+    
+    suite = TestSuite()
+    
+#     suite.addTest(HuiLifeResourceNicheCases("test_case"))  ## didi pages not display.
+#     suite.addTest(SquareResourceNicheCases("test_case"))
+#     suite.addTest(MovieTicketCases("test_case"))
+#     suite.addTest(SquareMovieCases("test_case"))
+    suite.addTest(ActivitySharingCases("test_case"))
+
+    
+    now = time.strftime('%H_%M_%S')
+    
+    filename = reportpath + 'feifan_automation_test_report.html'
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Feifan_automation_test_report',
+                                           description='Result for test')
+    runner.run(suite)
