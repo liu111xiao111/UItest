e30e7e7a70eab3ffe19c1f997a664fe94d106f04
maguowei1 <maguowei1@wanda.cn>
2016-07-15 08:48:40 +0800

    delete privilege_test_cases/smart_life_cases.py file
    Change-Id: Ie0bba004fc49946780244cb57ebe05ec87c4225a

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/routing_inspection_test_cases/all_cases.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/routing_inspection_test_cases/all_cases.py
@@ -88,7 +88,9 @@ if __name__ == "__main__":
 #     suite.addTest(PrivilegeCouponCases("test_case"))  ###绑定“水云间满额赠礼活动”，强依赖特定数据
 #     suite.addTest(SalesPromotionActiveCases("test_case"))
 #     suite.addTest(SalesPromotionCouponCases("test_case"))
+    
 #     suite.addTest(SpecialOfferCases("test_case")) ### 慧生活没有活动和优惠tab了，delete case
+
 #     suite.addTest(SquareGeneralCouponCases("test_case"))
 #     suite.addTest(SquareRecommendCases("test_case"))
 #     suite.addTest(FoodCases("test_case"))
@@ -104,15 +106,23 @@ if __name__ == "__main__":
 #     suite.addTest(ParkingBindingsCatergoryCases("test_case"))
 #     suite.addTest(ParkingBundingCatergoryCases("test_case"))
 #     suite.addTest(SquareParkingPaymentCases("test_case"))
-    
 #     suite.addTest(FeiFanCardBillCases("test_case"))
 #     suite.addTest(FeiFanCardIntegralCases("test_case"))
+    
 #     suite.addTest(FeiFanCardOpenCases("test_case")) #新版本不再有这个入口，用例删除掉
+
 #     suite.addTest(LogoutCases("test_case"))
 #     suite.addTest(MembershipCardPackageCases("test_case"))
 #     suite.addTest(MessageSettingsCases("test_case"))
 #     suite.addTest(MyfeifanMyLikeCases("test_case"))
-    suite.addTest(MyfeifanMyQueueCases("test_case"))
+#     suite.addTest(MyfeifanMyQueueCases("test_case"))
+#     suite.addTest(MyfeifanMyTicketCases("test_case"))
+#     suite.addTest(OneCardCases("test_case"))
+#     suite.addTest(PersonalInformationCases("test_case"))
+#     suite.addTest(SmallAmountPasswordLessPaymentCases("test_case"))
+#     suite.addTest(SquareMemberCases("test_case"))
+#     suite.addTest(SquareSignOnCases("test_case"))
+#     suite.addTest(UpdateLoginPasswordCases("test_case"))
     
     
     
