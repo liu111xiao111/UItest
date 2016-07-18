6317efa8264c35962b1709c9acbfb3a6b4367791
v_songbo1 <song_b@neusoft.com>
2016-07-18 15:29:25 +0800

    Add a new ios test case about no.1 splash screen home page.

---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/ios/ffan/routing_inspection_test_cases/splash_screen_home_page_cases.py
@@ -0,0 +1,51 @@
+# -*- coding:utf-8 -*-
+
+import os
+import time
+from unittest import TestCase
+from unittest import TestLoader
+
+import HTMLTestRunner
+
+from com.qa.automation.appium.cases.ios.ffan.common.clear_app_data import ClearAppData
+from com.qa.automation.appium.configs.ios_driver_configs import IosDriverConfigs as IDC
+from com.qa.automation.appium.driver.appium_driver import AppiumDriver
+from com.qa.automation.appium.pages.ios.ffan.splash_screen_home_page import SplashScreenHomePage
+from com.qa.automation.appium.utility.logger import Logger
+
+
+class SplashScreenHomePageCases(TestCase):
+    '''
+    巡检checklist No.: 01
+    自动化测试case No.: 01
+    启动APP，闪屏首页等正常显示
+    '''
+
+    def tearDown(self):
+        self.reset.clearData()
+        self.driver.quit()
+
+    def setUp(self):
+        self.logger = Logger()
+        self.driver = AppiumDriver(None,
+                                   None,
+                                   IDC.platformName,
+                                   IDC.platformVersion,
+                                   IDC.deviceName,
+                                   IDC.driverUrl,
+                                   IDC.bundleId,
+                                   IDC.udid).getDriver()
+
+        self.reset = ClearAppData(self.driver)
+
+    def test_case(self):
+        SplashScreenHomePage(self, self.driver, self.logger).validSelf()
+
+if __name__ == "__main__":
+    suite = TestLoader().loadTestsFromTestCase(SplashScreenHomePageCases)
+    now = time.strftime('%Y_%m_%d_%H_%M_%S')
+    reportpath = os.getcwd()
+    filename = reportpath + 'Feifan_automation_test_report_' + now + '.html'
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(fp, 'Feifan_automation_test_report', 'Result for test')
+    runner.run(suite)
---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/splash_screen_home_page.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+
+from com.qa.automation.appium.api.api import API
+from com.qa.automation.appium.pages.ios.common.super_page import SuperPage
+from com.qa.automation.appium.pages.ios.ffan.splash_screen_home_page_configs import SplashScreenHomePageConfigs
+
+
+class SplashScreenHomePage(SuperPage):
+    '''
+    This is a splash screen home page operation class.
+    '''
+
+    def __init__(self, testcase, driver, logger):
+        '''
+        Constructor
+        '''
+
+        super(SplashScreenHomePage, self).__init__(testcase, driver, logger)
+
+    def validSelf(self):
+        '''
+        usage: verify whether the current page is correct.
+        '''
+
+        API().assert_view_by_resourceID_Until(self.testcase, self.driver, self.logger,
+                                              SplashScreenHomePageConfigs.resource_id_skip_st,
+                                              SplashScreenHomePageConfigs.assert_view_timeout)
+
+if __name__ == '__main__':
+    pass
