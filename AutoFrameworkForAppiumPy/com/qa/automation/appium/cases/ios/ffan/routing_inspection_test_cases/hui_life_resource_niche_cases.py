d1d292bdccd59f4e74ddafdc5602a6af9d8647c2
v_songbo1 <song_b@neusoft.com>
2016-07-18 09:54:53 +0800

    Add a new case about no.39 hui life resource niche.

---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/ios/ffan/routing_inspection_test_cases/hui_life_resource_niche_cases.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import time
+from unittest import TestCase
+from unittest import TestLoader
+
+import HTMLTestRunner
+
+from com.qa.automation.appium.cases.ios.ffan.common.clear_app_data import ClearAppData
+from com.qa.automation.appium.cases.ios.ffan.common.test_prepare import TestPrepare
+from com.qa.automation.appium.configs.ios_driver_configs import IosDriverConfigs as IDC
+from com.qa.automation.appium.driver.appium_driver import AppiumDriver
+from com.qa.automation.appium.pages.ios.ffan.dashboard_page import DashboardPage
+from com.qa.automation.appium.pages.ios.ffan.hui_life_page import HuiLifePage
+from com.qa.automation.appium.utility.logger import Logger
+
+
+class HuiLifeResourceNicheCases(TestCase):
+    '''
+    巡检checklist No.: 39
+    自动化测试case No.: 39
+    首页-慧生活，惠生活截图，基本入口检查
+    '''
+
+    def tearDown(self):
+        self.driver.quit()
+        ClearAppData().clearData()
+
+    def setUp(self):
+        ClearAppData().clearData()
+        self.logger = Logger()
+        self.driver = AppiumDriver(None, None, IDC.platformName, IDC.platformVersion,
+                                   IDC.deviceName, IDC.driverUrl, IDC.bundleId, IDC.udid).getDriver()
+        TestPrepare(self, self.driver, self.logger).prepare()
+
+    def testHuiLifeScreenShot(self):
+        dashboardPage = DashboardPage(self, self.driver, self.logger)
+        dashboardPage.validSelf()
+        dashboardPage.clickOnHuiLife()
+
+        huiLifePage = HuiLifePage(self, self.driver, self.logger)
+        huiLifePage.validSelf()
+        huiLifePage.screen_shot("hui_life_resource_niche")
+
+        tempTuple = (u"滴滴出行", u"话费充值", u"加油",
+                     u"滴滴出行", u"流量充值", u"演唱会",
+                     u"滴滴出行", u"QQ充值", u"话剧",
+                     u"自选股", u"数码回收", u"音乐会",
+                     u"首页", u"有料", u"违章查询")
+        for tempNum in range(15):
+            huiLifePage.clickOnAndValidByXpathAndResourceId("//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAButton[%d]" % (tempNum + 1), tempTuple[tempNum])
+            huiLifePage.waitBySeconds()
+
+if __name__ == "__main__":
+    suite = TestLoader().loadTestsFromTestCase(HuiLifeResourceNicheCases)
+    now = time.strftime('%Y_%m_%d_%H_%M_%S')
+    reportpath = os.getcwd()
+    filename = reportpath + '/' + 'Hui_life_resource_niche_' + now + '.html'
+    fp = open(filename, 'wb')
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Feifan_automation_test_report', description='Result for test')
+    runner.run(suite)
---
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/hui_life_page.py
@@ -0,0 +1,378 @@
+# -*- coding:utf-8 -*-
+
+from com.qa.automation.appium.api.api import API
+from com.qa.automation.appium.pages.ios.common.super_page import SuperPage
+from com.qa.automation.appium.pages.ios.ffan.hui_life_page_configs import HuiLifePageConfigs
+
+HLPC = HuiLifePageConfigs()
+
+class HuiLifePage(SuperPage):
+    '''
+    首页->惠生活页面
+    '''
+
+    def __init__(self, testcase, driver, logger):
+        super(HuiLifePage, self).__init__(testcase,
+                                          driver,
+                                          logger)
+
+    def validSelf(self):
+        '''
+        usage: 验证惠生活界面
+        '''
+
+        API().assert_view_by_resourceID_Until(self.testcase, self.driver, self.logger,
+                                              HuiLifePageConfigs.resource_id_taking_taxi_st,
+                                              HuiLifePageConfigs.assert_view_timeout)
+
+    def clickOnAndValidByXpathAndResourceId(self, viewXpath, validValue):
+        '''
+        usage: 点击一个控件，并验证是否点击正确。
+        '''
+
+        print("KEYWORDS: %s" % validValue)
+        API().click_view_by_xpath(self.testcase, self.driver, self.logger, viewXpath,
+                                       HuiLifePageConfigs.click_on_button_timeout)
+        API().assert_view_by_resourceID_Until(self.testcase, self.driver, self.logger, validValue,
+                                              HuiLifePageConfigs.assert_view_timeout)
+        if u"滴滴出行" == validValue:
+            tempXpath = "//UIAApplication[1]/UIAWindow[1]/UIAButton[1]"
+        else:
+            tempXpath = "//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[2]"
+
+        API().click_back_key_ios(self.testcase, self.driver, self.logger, tempXpath,
+                                 HuiLifePageConfigs.click_on_button_timeout)
+
+    def clickOnActivity(self):
+        '''
+        usage: 点击活动
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_activity_button,
+                                         HLPC.click_on_button_timeout);
+
+    def clickOnPrivilege(self):
+        '''
+        usage: 点击优惠
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_privilege_button,
+                                         HLPC.click_on_button_timeout);
+
+    def clickOnSpecificActivity(self):
+        '''
+        usage: 点击特别活动按钮
+        '''
+        API().click_view_by_resourceID(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       HLPC.resource_id_specific_activity_button,
+                                       HLPC.click_on_button_timeout);
+
+    def clickOnSpecificPrivilege(self):
+        '''
+        usage: 点击特权
+        '''
+        API().click_view_by_resourceID(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       HLPC.resource_id_specific_privilege_button,
+                                       HLPC.click_on_button_timeout);
+
+
+    def clickOnResourceNiche(self):
+        '''
+        usage: 点击资源位
+        '''
+        API().click_view_by_resourceID(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       HLPC.resource_id_resource_niche_button,
+                                       HLPC.click_on_button_timeout)
+
+    def clickOnTaxi(self):
+        '''
+        usage: 点击打车
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_taxi,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnDesignatedDriving(self):
+        '''
+        usage: 点击代驾
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_designated_driving,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnBus(self):
+        '''
+        usage: 点击公交查询
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_bus,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnFeifanRead(self):
+        '''
+        usage: 点击飞凡阅读
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_feifan_read,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnFlyYue(self):
+        '''
+        usage: 点击飞悦
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_fly_yue,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnPrepaidRecharge(self):
+        '''
+        usage: 点击话费充值
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_prepaid_recharge,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnTrafficRecharge(self):
+        '''
+        usage: 点击流量充值
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_traffic_recharge,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnQQRecharge(self):
+        '''
+        usage: 点击QQ充值
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_qq_recharge,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnOnlineGameRecharge(self):
+        '''
+        usage: 点击网游充值
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_online_game_recharge,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnStockInformation(self):
+        '''
+        usage: 点击股票资讯
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_stock_information,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnRefuel(self):
+        '''
+        usage: 点击加油
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_refuel,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnConcert(self):
+        '''
+        usage: 点击演唱会
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_concert,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnDrama(self):
+        '''
+        usage: 点击话剧
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_drama,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnPhilharmonic(self):
+        '''
+        usage: 点击音乐会
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_philharmonic,
+                                         HLPC.click_on_button_timeout)
+
+    def clickOnIllegalInquiry(self):
+        '''
+        usage: 点击违章查询
+        '''
+        API().click_view_by_text_android(self.testcase,
+                                         self.driver,
+                                         self.logger,
+                                         HLPC.text_illegal_inquiry,
+                                         HLPC.click_on_button_timeout)
+
+    def validDiDiTravel(self):
+        '''
+        usage: 验证滴滴出行界面
+        '''
+        API().assert_view_by_resourceID_Until(self.testcase,
+                                              self.driver,
+                                              self.logger,
+                                              HLPC.verify_resource_didi_travel,
+                                              HLPC.assert_view_timeout)
+
+    def validFeifanRead(self):
+        '''
+        usage: 验证飞凡阅读界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_feifan_read,
+                                          HLPC.assert_view_timeout)
+
+    def validFlyYue(self):
+        '''
+        usage: 验证飞悦界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_fly_yue,
+                                          HLPC.assert_view_timeout)
+
+    def validPrepaidRecharge(self):
+        '''
+        usage: 验证话费充值界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_prepaid_recharge,
+                                          HLPC.assert_view_timeout)
+
+    def validTrafficRecharge(self):
+        '''
+        usage: 验证流量充值界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_traffic_recharge,
+                                          HLPC.assert_view_timeout)
+
+    def validQQRecharge(self):
+        '''
+        usage: 验证QQ充值界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_qq_recharge,
+                                          HLPC.assert_view_timeout)
+
+    def validOnlineGameRecharge(self):
+        '''
+        usage: 验证网游充值界面
+        '''
+        API().assert_view_by_text_android(self.testcase,
+                                          self.driver,
+                                          self.logger,
+                                          HLPC.verify_text_online_game_recharge,
+                                          HLPC.assert_view_timeout)
+
+    def validStockInformation(self):
+        '''
+        usage: 验证股票资讯界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_stock_information,
+                                                  HLPC.assert_view_timeout)
+
+    def validRefuel(self):
+        '''
+        usage: 验证加油界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_refuel,
+                                                  HLPC.assert_view_timeout)
+
+    def validConcert(self):
+        '''
+        usage: 验证演唱会界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_concert,
+                                                  HLPC.assert_view_timeout)
+
+    def validDrama(self):
+        '''
+        usage: 验证话剧界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_drama,
+                                                  HLPC.assert_view_timeout)
+
+    def validPhilharmonic(self):
+        '''
+        usage: 验证音乐会界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_philharmonic,
+                                                  HLPC.assert_view_timeout)
+
+    def validIllegalInquiry(self):
+        '''
+        usage: 验证违章查询界面
+        '''
+        API().assert_view_by_content_desc_android(self.testcase,
+                                                  self.driver,
+                                                  self.logger,
+                                                  HLPC.verify_text_illegal_inquiry,
+                                                  HLPC.assert_view_timeout)
+
+if __name__ == '__main__':
+    pass
