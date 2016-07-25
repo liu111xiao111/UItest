cde7b8e6d4e06b2841c0dd1cb4a2cd2fde9e3f28
v_songbo1 <song_b@neusoft.com>
2016-07-25 20:17:27 +0800

    Update food pages

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/android/ffan/food_category_page.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/android/ffan/food_category_page.py
@@ -1,86 +1,91 @@
 # -*- coding: utf-8 -*-
 
-from com.qa.automation.appium.pages.android.ffan.food_category_page_configs import FoodCategoryPageConfigs
+from com.qa.automation.appium.api.api_new import API
 from com.qa.automation.appium.pages.android.common.super_page import SuperPage
-from com.qa.automation.appium.api.api import API
+from com.qa.automation.appium.pages.android.ffan.food_category_page_configs import FoodCategoryPageConfigs as FCPC
 
-FCPC = FoodCategoryPageConfigs()
 
-#   首页点击 美食
 class FoodCategoryPage(SuperPage):
+    '''
+    作者 刘涛
+    首页＝>美食汇
+    '''
     def __init__(self, testcase, driver, logger):
-        super(FoodCategoryPage, self).__init__(testcase=testcase, driver=driver, logger=logger);
+        super(FoodCategoryPage, self).__init__(testcase, driver, logger)
 
     '''
         usage: 美食主界面，根据美食种类button入口，检查美食主页面是否加载出来.
     '''
     def validFoodHomePage(self):
-        API().assert_view_by_text_android(self.testcase,
-                                          self.driver,
-                                          self.logger,
-                                          FCPC.view_text_tiltle,
-                                          FCPC.verify_view_timeout);
+        '''
+        usage: 美食主界面，根据美食种类button入口，检查美食主页面是否加载出来.
+        '''
+        API().assertElementByText(self.testcase,
+                                  self.driver,
+                                  self.logger,
+                                  FCPC.view_text_tiltle,
+                                  FCPC.verify_view_timeout)
 
-    '''
-        usage : 进入美食子页面，根据餐饮的textview, 检查找餐饮页面是否加载出来.
-    '''
     def validRestaurant(self):
-        API().assert_view_by_resourceID_Until(self.testcase,
-                                              self.driver,
-                                              self.logger,
-                                              FCPC.resource_id_tv_restaurant_tv,
-                                              FCPC.verify_view_timeout);
+        '''
+        usage : 进入美食子页面，根据餐饮的textview, 检查找餐饮页面是否加载出来.
+        '''
+        API().assertElementByResourceId(self.testcase,
+                                        self.driver,
+                                        self.logger,
+                                        FCPC.resource_id_tv_restaurant_tv,
+                                        FCPC.verify_view_timeout)
 
-    '''
-        usage : 进入优惠打折界面，根据餐饮的textview, 检查找优惠页面是否加载出来.
-    '''
     def validCoupon(self):
-        API().assert_view_by_resourceID_Until(self.testcase,
-                                              self.driver,
-                                              self.logger,
-                                              FCPC.resource_id_tv_restaurant_tv,
-                                              FCPC.verify_view_timeout);
-    
-    '''
-        usage: 进入抢券界面，根据餐饮的textview, 检查抢券界面是否加载出来.
-    '''
-    def validGrabCoupons(self):
-        API().assert_view_by_resourceID_Until(self.testcase,
-                                              self.driver,
-                                              self.logger,
-                                              FCPC.resource_id_tv_restaurant_tv,
-                                              FCPC.verify_view_timeout);
+        '''
+        usage : 进入优惠打折界面，根据餐饮的textview, 检查找优惠页面是否加载出来.
+        '''
+        API().assertElementByResourceId(self.testcase,
+                                        self.driver,
+                                        self.logger,
+                                        FCPC.resource_id_tv_restaurant_tv,
+                                        FCPC.verify_view_timeout)
 
-    '''
-        usage: 点击美食主界面的所有入口并验证
-    '''
+    def validGrabCoupons(self):
+        '''
+        usage: 进入抢券界面，根据餐饮的textview, 检查抢券界面是否加载出来.
+        '''
+        API().assertElementByResourceId(self.testcase,
+                                        self.driver,
+                                        self.logger,
+                                        FCPC.resource_id_tv_restaurant_tv,
+                                        FCPC.verify_view_timeout)
+
     def validModules(self):
-        restaurantList = API().get_views_by_resourceID(self.driver,
+        '''
+        usage: 点击美食主界面的所有入口并验证
+        '''
+        restaurantList = API().getElementsByResourceId(self.testcase,
+                                                       self.driver,
                                                        self.logger,
-                                                       FCPC.resource_id_bt_restaurant_bt)
+                                                       FCPC.resource_id_bt_restaurant_bt,
+                                                       FCPC.verify_view_timeout)
         for restaurant in restaurantList:
             restaurant.click()
             self.validRestaurant()
             self.clickBackKey()
 
-    '''
-        usage : 点击优惠打折
-    '''
     def clickOnCoupon(self):
-        API().click_view_by_resourceID(self.testcase,
+        '''
+        usage : 点击优惠打折
+        '''
+        API().clickElementByResourceId(self.testcase,
                                        self.driver,
                                        self.logger,
-                                       FCPC.resource_id_bt_coupon_bt);
+                                       FCPC.resource_id_bt_coupon_bt,
+                                       FCPC.click_view_timeout)
 
-    '''
-        usage : 点击抢券
-    '''
     def clickOnGrabCoupons(self):
-        API().click_view_by_resourceID(self.testcase,
+        '''
+        usage : 点击抢券
+        '''
+        API().clickElementByResourceId(self.testcase,
                                        self.driver,
                                        self.logger,
-                                       FCPC.resource_id_bt_grab_bt);
-
-
-if __name__ == '__main__':
-    pass;
+                                       FCPC.resource_id_bt_grab_bt,
+                                       FCPC.click_view_timeout)
