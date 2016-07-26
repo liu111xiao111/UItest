870cd3cc94eef20ca92bc699af5b647f0f3b2cee
v_songbo1 <song_b@neusoft.com>
2016-07-26 11:04:32 +0800

    Update switch city and version update case pages

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/android/ffan/version_upgrade_page.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/android/ffan/version_upgrade_page.py
@@ -2,74 +2,82 @@
 
 from selenium.common.exceptions import TimeoutException
 
-from com.qa.automation.appium.api.api import API
+from com.qa.automation.appium.api.api_new import API
 from com.qa.automation.appium.pages.android.common.super_page import SuperPage
-from com.qa.automation.appium.pages.android.ffan.version_upgrade_page_configs import VersionUpgradePageConfigs
+from com.qa.automation.appium.pages.android.ffan.version_upgrade_page_configs import VersionUpgradePageConfigs as VUPC
 
 
 class VersionUpgradePage(SuperPage):
     '''
-    This is a version update page operation class.
+    作者 宋波
+    版本更新
     '''
-
     def __init__(self, testcase, driver, logger):
-        '''
-        Constructor
-        '''
-
         super(VersionUpgradePage, self).__init__(testcase, driver, logger)
 
     def validSelf(self, assertable=True):
         '''
-        usage: verify whether the current page is the version upgrade page.
+        usage: 验证版本更新
         '''
 
         if assertable:
-            API().assert_view_by_resourceID_Until(self.testcase, self.driver, self.logger,
-                                                          VersionUpgradePageConfigs.resource_id_upgrade_cancel_button,
-                                                          VersionUpgradePageConfigs.assert_view_timeout)
+            API().assertElementByResourceId(self.testcase,
+                                            self.driver,
+                                            self.logger,
+                                            VUPC.resource_id_upgrade_cancel_button,
+                                            VUPC.assert_view_timeout)
             return True
         else:
-            try:
-                API().find_view_by_resourceID_Until_android(self.driver, self.logger,
-                                                            VersionUpgradePageConfigs.resource_id_upgrade_cancel_button,
-                                                            VersionUpgradePageConfigs.verify_view_timeout)
+            res = API().validElementByResourceId(self.driver,
+                                           self.logger,
+                                           VUPC.resource_id_upgrade_cancel_button,
+                                           VUPC.verify_view_timeout)
+            if res:
                 return True
-            except TimeoutException:
-                return False
+            return False
 
     def cancelVersionUpgrade(self):
         '''
-        usage: cancel version upgrade.
+        usage: 取消版本更新
         '''
 
-        API().click_view_by_resourceID(self.testcase, self.driver, self.logger,
-                                               VersionUpgradePageConfigs.resource_id_upgrade_cancel_button)
+        API().clickElementByResourceId(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       VUPC.resource_id_upgrade_cancel_button,
+                                       VUPC.verify_view_timeout)
 
     def confirmVersionUpgrade(self):
         '''
-        usage: confirm version upgrade.
+        usage: 确认版本更新
         '''
 
-        API().click_view_by_resourceID(self.testcase, self.driver, self.logger,
-                                               VersionUpgradePageConfigs.resource_id_upgrade_confirm_button)
+        API().clickElementByResourceId(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       VUPC.resource_id_upgrade_confirm_button,
+                                       VUPC.verify_view_timeout)
 
     def validPercentage(self):
         '''
-        usage: verity whether the percentage is 100%.
+        usage: 验证进度
         '''
 
-        API().find_view_by_text_Until_android(self.testcase, self.driver, self.logger, "100%", 600)
+        API().assertElementByText(self.testcase,
+                                  self.driver,
+                                  self.logger,
+                                  "100%",
+                                  600)
 
     def invalidSelf(self):
         '''
-        usage: verify whether the current page is not the version upgrade page.
+        usage: 验证当前页面不是版本更新页面
         '''
 
-        API().assert_none_view_by_resource_id_until_android(self.testcase, self.driver, self.logger,
-                                                            VersionUpgradePageConfigs.resource_id_upgrade_cancel_button,
-                                                            VersionUpgradePageConfigs.assert_invalid_view_time)
-
-
-if __name__ == '__main__':
-    pass
+        API().assertFalse(self.testCase,
+                          self.logger,
+                          API().validElementByResourceId(self.testcase,
+                                                         self.driver,
+                                                         self.logger,
+                                                         VUPC.resource_id_upgrade_cancel_button,
+                                                         VUPC.assert_invalid_view_time))
--- AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/switch_city_page.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/pages/ios/ffan/switch_city_page.py
@@ -2,7 +2,7 @@
 
 from com.qa.automation.appium.api.api_new import API
 from com.qa.automation.appium.pages.android.common.super_page import SuperPage
-from com.qa.automation.appium.pages.ios.ffan.switch_city_page_configs import SwitchCityPageConfigs
+from com.qa.automation.appium.pages.ios.ffan.switch_city_page_configs import SwitchCityPageConfigs as SCPC
 
 
 class SwitchCityPage(SuperPage):
@@ -12,54 +12,56 @@ class SwitchCityPage(SuperPage):
     '''
 
     def __init__(self, testcase, driver, logger):
-        '''
-        Constructor
-        '''
-
         super(SwitchCityPage, self).__init__(testcase, driver, logger)
 
     def validSelf(self, assertable=True):
         '''
-        usage: verify whether the current page is the switch city page.
+        usage:验证城市切换
         '''
 
         if assertable:
-            API().assertElementByResourceId(self.testcase, self.driver, self.logger,
-                                            SwitchCityPageConfigs.resource_id_switch_city_cancel_bt,
-                                            SwitchCityPageConfigs.assert_view_timeout)
+            API().assertElementByResourceId(self.testcase,
+                                            self.driver,
+                                            self.logger,
+                                            SCPC.resource_id_switch_city_cancel_bt,
+                                            SCPC.assert_view_timeout)
             return True
         else:
-            return API().validElementByResourceId(self.driver, self.logger,
-                                                  SwitchCityPageConfigs.resource_id_switch_city_cancel_bt,
-                                                  SwitchCityPageConfigs.verify_view_timeout)
+            return API().validElementByResourceId(self.driver,
+                                                  self.logger,
+                                                  SCPC.resource_id_switch_city_cancel_bt,
+                                                  SCPC.verify_view_timeout)
 
     def cancelSwitchCity(self):
         '''
-        usage: cancel switch city.
+        usage: 取消城市切换
         '''
 
-        API().clickElementByResourceId(self.testcase, self.driver, self.logger,
-                                       SwitchCityPageConfigs.resource_id_switch_city_cancel_bt,
-                                       SwitchCityPageConfigs.click_on_button_timeout)
+        API().clickElementByResourceId(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       SCPC.resource_id_switch_city_cancel_bt,
+                                       SCPC.click_on_button_timeout)
 
     def confirmSwitchCity(self):
         '''
-        usage: confirm switch city.
+        usage: 确认城市切换
         '''
 
-        API().click_view_by_resourceID(self.testcase, self.driver, self.logger,
-                                       SwitchCityPageConfigs.resource_id_switch_city_switch_bt,
-                                       SwitchCityPageConfigs.click_on_button_timeout)
+        API().click_view_by_resourceID(self.testcase,
+                                       self.driver,
+                                       self.logger,
+                                       SCPC.resource_id_switch_city_switch_bt,
+                                       SCPC.click_on_button_timeout)
 
     def invalidSelf(self):
         '''
-        usage: verify whether the current page is not the switch city page.
+        usage: 验证当前界面不是选择城市界面
         '''
 
-        API().assertFalse(self.testcase, self.logger,
-                          API().validElementByResourceId(self.driver, self.logger,
-                                                         SwitchCityPageConfigs.resource_id_switch_city_cancel_bt,
-                                                         SwitchCityPageConfigs.assert_invalid_view_time))
-
-if __name__ == '__main__':
-    pass
+        API().assertFalse(self.testcase,
+                          self.logger,
+                          API().validElementByResourceId(self.driver,
+                                                         self.logger,
+                                                         SCPC.resource_id_switch_city_cancel_bt,
+                                                         SCPC.assert_invalid_view_time))
