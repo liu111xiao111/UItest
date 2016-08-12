76ed8f9f1f90f7d1f2e4fa801b028a654a0f24ed
v_liutao <tl@neusoft.com>
2016-08-11 17:12:33 -0700

    Update performance test case by Neusoft

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/performance_test_cases/BootTime.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/performance_test_cases/BootTime.py
@@ -1,24 +1,23 @@
 import os
 import time
 import subprocess
+from com.qa.automation.appium.configs.driver_configs import appPackage_ffan
+from com.qa.automation.appium.configs.driver_configs import appActivity_ffan
 
-global path,activity,package
-# path = "G:\\performance\\"
-path = "C:\\"
-activity = raw_input("please input activity: ")
-package = raw_input("please input package: ")
+now = time.strftime('%Y%m%d%H%M%S')
+resourcesDirectory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(
+                                os.path.dirname(os.path.abspath(__file__)))))) + "/resources/"
 
-def getWarmBootTime(package,activity):
-	
-	print "Start the WarmBoot test, please open the file until test complete"
-	# startTime = "am start -W " + package + "/" + activity + " | grep -E " + "\'" + "TotalTime|WaitTime" + "\'" 
-	bootTime = "am start -W " + package + "/" + activity + " | grep TotalTime"
-	cmdam = "adb shell " + "\"" + bootTime + "\""
+
+def getWarmBootTime():
+
+	print("\nStart the WarmBoot test, please open the file until test complete")
+	bootTime = "am start -W " + appPackage_ffan + "/" + appActivity_ffan + " | grep TotalTime"
+	cmdam = "%sadb shell \"%s\"" % (resourcesDirectory, bootTime)
 	keycode = "keyevent KEYCODE_BACK"
-	cmdBack = "adb shell input " + keycode
-	now = time.strftime('%Y%m%d%H%M%S')
-	f1 = open(path + "WarmBootTime_" + package + "_" + now + ".txt", "a")
-	for x in xrange(0,10):
+	cmdBack = "%sadb shell input %s" % (resourcesDirectory, keycode)
+	f1 = open("WarmBootTime_" + appPackage_ffan + "_" + now + ".txt", "a")
+	for x in range(0,10):
 		os.system(cmdBack)
 		time.sleep(0.3)
 		os.system(cmdBack)
@@ -26,7 +25,7 @@ def getWarmBootTime(package,activity):
 		pipe = subprocess.Popen(cmdam, shell=True, stdout = f1).stdout
 		time.sleep(5)
 	f1.close()
-	f2 = open(path + "WarmBootTime_" + package + "_" + now + ".txt", "r")
+	f2 = open("WarmBootTime_" + appPackage_ffan + "_" + now + ".txt", "r")
 	allTime = []
 	originals = f2.readlines()
 	f2.close
@@ -41,34 +40,31 @@ def getWarmBootTime(package,activity):
 	for i in range (len(allTime)):
 		finalTime = int (allTime[i]) + finalTime
 		time.sleep(1)
-	# print finalTime
 	averageTime = finalTime/10
-	# print averageTime
-	f = open(path + "WarmBootTime_" + package + "_" + now + ".txt", "a")
-	f.write("TotalTime: " + str(finalTime) + "\n")
-	f.write("AverageTime: " + str(averageTime))
+	logName = "WarmBootTime_" + appPackage_ffan + "_" + now + ".txt"
+	f = open(logName, "a")
+	f.write("\nTotal Time: " + str(finalTime) + "\n")
+	f.write("Average Time: " + str(averageTime))
 	time.sleep(1)
 	f.close
-	print "getWarmBootTime Test complete, go to " + path + " and see the details"
+	print("getWarmBootTime Test complete, go to " + logName + " and see the details")
 
 
+def getColdBootTime():
 
-def getColdBootTime(package,activity):
-
-	print "Start the ColdBoot test, please open the file until test complete"
-	bootTime = "am start -W " + package + "/" + activity + " | grep TotalTime"
-	cmdam = "adb shell " + "\"" + bootTime + "\""
-	kill = "am force-stop " + package 
-	cmdKill = "adb shell " + kill
-	now = time.strftime('%Y%m%d%H%M%S')
-	f1 = open(path + "ColdBootTime_" + package + "_" + now + ".txt", "a")
-	for x in xrange(0,10):
+	print("\nStart the ColdBoot test, please open the file until test complete")
+	bootTime = "am start -W " + appPackage_ffan + "/" + appActivity_ffan + " | grep TotalTime"
+	cmdam = "%sadb shell \"%s\"" % (resourcesDirectory, bootTime)
+	kill = "am force-stop " + appPackage_ffan 
+	cmdKill = "%sadb shell \"%s\"" % (resourcesDirectory, kill)
+	f1 = open("ColdBootTime_" + appPackage_ffan + "_" + now + ".txt", "a")
+	for x in range(0,10):
 		os.system(cmdKill)
 		time.sleep(10)
 		pipe = subprocess.Popen(cmdam, shell=True, stdout = f1).stdout
 		time.sleep(10)
 	f1.close()
-	f2 = open(path + "ColdBootTime_" + package + "_" + now + ".txt", "r")
+	f2 = open("ColdBootTime_" + appPackage_ffan + "_" + now + ".txt", "r")
 	allTime = []
 	originals = f2.readlines()
 	f2.close
@@ -83,17 +79,16 @@ def getColdBootTime(package,activity):
 	for i in range (len(allTime)):
 		finalTime = int (allTime[i]) + finalTime
 		time.sleep(1)
-	# print finalTime
 	averageTime = finalTime/10
-	# print averageTime
-	f = open(path + "ColdBootTime_" + package + "_" + now + ".txt", "a")
-	f.write("TotalTime: " + str(finalTime) + "\n")
-	f.write("AverageTime: " + str(averageTime))
+	logName = "ColdBootTime_" + appPackage_ffan + "_" + now + ".txt"
+	f = open(logName, "a")
+	f.write("\nTotal Time: " + str(finalTime) + "\n")
+	f.write("Average Time: " + str(averageTime))
 	time.sleep(1)
 	f.close
 
-	print "getColdBootTime Test complete, go to " + path + " and see the details"
+	print("getColdBootTime Test complete, go to " + logName + " and see the details")
 
 
-getColdBootTime(package,activity)
-getWarmBootTime(package,activity)
+getColdBootTime()
+getWarmBootTime()
