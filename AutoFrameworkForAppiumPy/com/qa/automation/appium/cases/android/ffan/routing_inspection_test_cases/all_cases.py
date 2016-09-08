1ff4eff3cecf285b75be58aac5f30ddeb14ecbee
v_songbo1 <song_b@neusoft.com>
2016-09-08 17:01:00 +0800

    Update all cases

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/routing_inspection_test_cases/all_cases.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/cases/android/ffan/routing_inspection_test_cases/all_cases.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
 
     #root_dir = os.path.dirname(
     #    os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
-    reportpath = "%s/report/ffan/%s/%s/" % ("/Users/yiceyun/work", time.strftime("%Y%m%d"), build_num)
+    reportpath = "%s/report/ffan/%s/%s/" % ("/Users/ds/jenkins/workspace/android_allcaseauto/autotest/AutoFrameworkForAppiumPy", time.strftime("%Y%m%d"), build_num)
     if not os.path.exists(reportpath):
         os.makedirs(reportpath)
 
