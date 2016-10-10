e4d65568c059f0ce70931b112c88e38ce30b66d9
v_songbo1 <song_b@neusoft.com>
2016-10-10 17:55:38 +0800

    Update mail list. @

--- AutoFrameworkForAppiumPy/com/qa/automation/appium/configs/constants.py
+++ AutoFrameworkForAppiumPy/com/qa/automation/appium/configs/constants.py
@@ -10,15 +10,10 @@ class Email():
     username = 'yiceyun'
     password = '1qaz!QAZ'
     smtpPort = '587'
-    
-    '''
+
     userMailAddress = ['sunkai31@wanda.cn', 'chencheng@neusoft.com', 'song_b@neusoft.com', 'tl@neusoft.com',
                        'qiaojx@neusoft.com', 'lichunyan19@wanda.cn', 'liudan71@wanda.cn', '1284387741@qq.com',
                        'fengzc@neusoft.com', 'zh-ying@neusoft.com', 'cui.yue@neusoft.com', 'zhao.xu_neu@neusoft.com',
                        'w.xin_neu@neusoft.com', 'wang-jinpeng@neusoft.com', 'jiangw-neu@neusoft.com',
                        'li_ys@neusoft.com', 'zhangshihao@neusoft.com', 'shang.jj@neusoft.com', 'huang.shuo@neusoft.com',
                        'yang.hch@neusoft.com', 'zhaocj@neusoft.com', 'luxiangyang@neusoft.com']
-
-    '''
-    
-    userMailAddress = ['song_b@neusoft.com', 'tl@neusoft.com']
