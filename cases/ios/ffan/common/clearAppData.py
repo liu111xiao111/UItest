9fa19298e716597710d43b857c38e3087417424b
v_chencheng <chencheng@neusoft.com>
2016-10-19 15:59:20 +0800

    add clear app data function@

---
+++ cases/ios/ffan/common/clearAppData.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+'''
+    usage :  清除App数据
+'''
+class ClearAppData:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def clearData(self):
+#         self.driver.reset()
+        pass
