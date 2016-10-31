24b6c7133890e9a1ea4727361098bd810c13d6eb
v_liutao <tl@neusoft.com>
2016-10-31 18:02:55 +0800

    1,Add 'ren yuan lie biao' case 2,Add 'xin zeng yuan gong' case

--- pages/ios/shanghu/employeeModulePage.py
+++ pages/ios/shanghu/employeeModulePage.py
@@ -1,3 +1,69 @@
+# -*- coding:utf-8 -*-
+
+from pages.ios.common.superPage import SuperPage
+from api.api import API
+from pages.ios.shanghu.shanghuPageConfig import Xpath
+from pages.ios.shanghu.shanghuPageConfig import Name
+from pages.ios.shanghu.shanghuPageConfig import Text
 
 
+class EmployeeModulePage(SuperPage):
+
+
+    def checkEmployeeList(self):
+        '''
+        检查员工列表,员工姓名,手机号等是否为空
+        :return:
+        '''
+
+        name = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_name)
+        sotreName = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_store_name)
+        role = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_role)
+        phone = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_phone)
+        chuangjianren = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_chuangjianren)
+        date = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_date)
+
+        API().assertTrue(self.testcase, self.logger, not name is None)
+        API().assertTrue(self.testcase, self.logger, not sotreName is None)
+        API().assertTrue(self.testcase, self.logger, not role is None)
+        API().assertTrue(self.testcase, self.logger, not phone is None)
+        API().assertTrue(self.testcase, self.logger, not chuangjianren is None)
+        API().assertTrue(self.testcase, self.logger, not date is None)
+
+    def clickAddEmployeeButton(self):
+        '''
+        点击新增员工按钮
+        :return:
+        '''
+        API().clickElementByName(self.testcase, self.driver, self.logger, Name.add_new_employee)
+
+    def selectRole(self):
+        '''
+        新增员工,选择角色
+        :return:
+        '''
+        API().clickElementByXpath(self.testcase, self.driver, self.logger, Xpath.new_employee_please_selected_role)
+        #选择店长角色
+        API().clickElementByXpath(self.testcase, self.driver, self.logger, Xpath.new_employee_select_role_radio_button)
+        API().clickElementByName(self.testcase, self.driver, self.logger, Name.save_button)
+
+    def inputUserNameAndPassword(self):
+        '''
+        输入姓名和电话号
+        :return:
+        '''
+        API().inputStringByXpath(self.testcase, self.driver, self.logger, Xpath.new_employee_input_name, Text.new_employee_name)
+        API().inputStringByXpath(self.testcase, self.driver, self.logger, Xpath.new_employee_input_phone_name,Text.new_employee_phone)
+
+        #保存
+        API().clickElementByName(self.testcase, self.driver, self.logger, Name.save_button)
+
+    def checkNewUserStatus(self):
+        '''
+        检查是否添加新员工成功
+        :return:
+        '''
+        name = API().getTextByXpath(self.testcase, self.driver, self.logger, Xpath.employee_module_normal_name)
+
+        API().assertTrue(self.testcase, self.logger, name==Text.new_employee_name)
 
--- pages/ios/shanghu/shanghuPageConfig.py
+++ pages/ios/shanghu/shanghuPageConfig.py
@@ -16,6 +16,24 @@ class Xpath:
     #首页,登录信息按钮
     personalInfo = "//UIAApplication[1]/UIAWindow[1]/UIANavigationBar[1]/UIAButton[4]"
 
+    #员工管理,正常状态,员工姓名
+    employee_module_normal_name = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[1]"
+    employee_module_normal_store_name = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[2]"
+    employee_module_normal_role = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[4]"
+    employee_module_normal_phone = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[8]"
+    employee_module_normal_chuangjianren = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[6]"
+    employee_module_normal_date = "//UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[1]/UIAStaticText[10]"
+
+    #新增加员工,选择角色
+    new_employee_please_selected_role = "//UIAApplication[1]/UIAWindow[1]/UIAButton[2]"
+    #新增加员工,选择角色列表,最后一个
+    new_employee_select_role_radio_button = " //UIAApplication[1]/UIAWindow[1]/UIATableView[1]/UIATableCell[9]/UIAButton[1]"
+    #新增加员工,输入姓名
+    new_employee_input_name = "//UIAApplication[1]/UIAWindow[1]/UIATextField[2]"
+    #新增加员工,输入电话号
+    new_employee_input_phone_name = "//UIAApplication[1]/UIAWindow[1]/UIATextField[3]"
+
+
 
 
 
@@ -29,13 +47,26 @@ class Name:
     userName = u"商户下的员工"
     userIdentity = u"QA线上验证专用权限组"
     userStore = u"QA线上测试用普通商户"
+    employeeManager = u"员工管理"
 
+    add_new_employee = u"新增员工"
+
+
+    save_button = u"保存"
+    please_input_name = u"请输入姓名"
 
 class Text:
     phoneNumber = u"15624958068"
     password = u"wanda123"
     #密码框初始文字
     initial_password = u"请输入密码(长度8-20位)"
+
+    #默认密码
+    default_password = u"Abc123456"
+    #新增员工名字
+    new_employee_name = u"王Test"
+    new_employee_phone = "13504286090"
+
 
 
 
