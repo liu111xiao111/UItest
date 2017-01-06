2e15b1a9c438dde12d55051cb50fa048369d681c
v_songbo1 <song_b@neusoft.com>
2017-01-06 16:32:36 +0800

    Add base class @

---
+++ cases/base.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+from unittest import TestCase
+
+
+class TEBase(TestCase):
+    def __init__(self):
+        self.driver = None
+        self.logPath = None
+
+    def setUp(self):
+        pass
+
+    def test(self):
+        pass
+
+    def tearDown(self):
+        pass
